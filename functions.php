96f7f65cf76f01e542d15a9e7b9c0cac97eaead6
Marv <marv@marv-ThinkPad-SL510.(none)>
2013-04-10 23:59:53 +0200

    Großteil des Portfolios automatisiert eingefügt. Content aktuell noch über 'the_content()' geladen, wird noch so weit wie möglich ersetzt.

--- functions.php
+++ functions.php
@@ -133,40 +133,66 @@ function contributors() {
 // --- To exclude a Profile set display_name = inactive --- //
 function portfolio() {
 
+	global $wpdb;
+		$users = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users WHERE display_name = 'bine'");
+
+	foreach ($users as $user ) {
+		echo "<h1 style='text-align: center; font-size: 40px;'>";
+		the_author_meta('city', $user->ID);
+		echo " (";
+		the_author_meta('first_name', $user->ID);
+		echo ")</h1>";
+
+		echo "<div class='contact-wrapper'>";
+		echo "<div class='contact-left1'>";
+		echo "<div class='layer-0'>";
+		echo "<h2>E-Mail-Beratung<br />";
+			echo "<a href='mailto:";
+			the_author_meta('user_email', $user->ID);
+			echo "'>";
+			the_author_meta('user_email', $user->ID);
+			echo "</a></h2>";
+		echo "<p>Durchschnittliche Reaktionszeit: 48h</p>";
+		echo "</p></div>";
+		echo "</p></div>";
+		echo "<div class='contact-left2'>";
+		echo "<div class='layer-0 layer-1'>";
+		echo "<h3>Telefon &#8211; Beratung<br />";
+			echo "0178 &#8211; 1445533<br />";
+			echo "Montags bis Freitags von 12-18 Uhr</h3>";
+		echo "</p></div>";
+		echo "<p>      Hinweise: </p>";
+		echo "<ul>";
+		echo "<li>jss salkd jklsdj asdlkjasl</li>";
+		echo "<li>uewhrkh hrjk ehkjh kjr</li>";
+		echo "</ul></div>";
+		echo "</div>";
+		echo "<div class='informationen'>";
+		echo "<h2>Informationen</h2>";
+/*		// regular expression:
+		$matchSrc = "/src=[\"' ]?([^\"' >]+)[\"' ]?[^>]*>/i" ;
+		// get the <img src="... from the plugin, via one on various methods explained in the documentation...
+		$avatar = get_avatar($user->ID, 64); // for example
+		// preg match to extract the src= bit
+		preg_match($matchSrc, $avatar, $matches);
+		$theImageUrl = $matches[1];
+		echo "<img src='$theImageUrl' alt='PageLines- Marvin.jpg' width='150' height='225' class='alignright size-full wp-image-7985' />";*/
+the_content();
+/*		the_author_meta('infotext', $user->ID); */
+		echo "</div>";
+		echo "<div class='portfolio'>";
+		echo "<h2>Portfolio</h2>";
+		echo "[Portfolio]";
+		echo "</div>";
+		echo "<div class='workshops'>";
+		echo "<h2>Workshops</h2>";
+		echo "<p>-DreadFactory Treffen 2012";
+		echo "</p></div>";
+
+
+	}
+
 ?>
-<h1 style="text-align: center; font-size: 40px;">Vertrieb und IT (Marv)</h1>
-<div class="contact-wrapper">
-<div class="contact-left1">
-<div class="layer-0">
-<h2>E-Mail-Beratung<br />
-      <a href="mailto:marv@dreadfactory.de">marv@dreadfactory.de</a></h2>
-<p>Durchschnittliche Reaktionszeit: 48h</p>
-</p></div>
-</p></div>
-<div class="contact-left2">
-<div class="layer-0 layer-1">
-<h3>Telefon &#8211; Beratung<br />
-      0178 &#8211; 1445533<br />
-      Montags bis Freitags von 12-18 Uhr</h3>
-</p></div>
-<p>      Hinweise: </p>
-<ul>
-<li>jss salkd jklsdj asdlkjasl</li>
-<li>uewhrkh hrjk ehkjh kjr</li>
-</ul></div>
-</div>
-<div class="informationen">
-<h2>Informationen</h2>
-<p><a href="http://it.marvin-pollock.com/wp-content/uploads/2012/05/Marvin1.jpg"><img src="http://it.marvin-pollock.com/wp-content/uploads/2012/05/Marvin1.jpg" alt="PageLines- Marvin.jpg" width="150" height="225" class="alignright size-full wp-image-7985" /></a>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu lacus massa. In vehicula turpis ut mi hendrerit id bibendum sem consectetur. In sagittis luctus neque vitae eleifend. In lectus erat, tincidunt ac laoreet ac, consectetur sed orci. Ut a sagittis nibh. Fusce lectus erat, ultrices et suscipit id, sollicitudin porttitor orci. Praesent id lacus risus, eu aliquam lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent eget mauris est, nec faucibus ligula. Quisque sagittis viverra accumsan. Quisque luctus, erat eu convallis consequat, nulla felis feugiat ante, et dignissim ligula nibh quis orci. Fusce congue luctus dapibus. Morbi posuere convallis blandit.</p>
-<p>Nulla auctor pharetra nisi ut iaculis. Sed convallis, quam ac condimentum varius, neque libero tempus libero, vitae elementum libero turpis a diam. Nullam lobortis magna nec orci rhoncus consequat. Aenean lacinia aliquam lorem, nec adipiscing tellus placerat vel. Donec leo enim, sollicitudin sit amet elementum eu, condimentum et sem. Phasellus interdum eleifend bibendum. Praesent vitae aliquet erat. Aliquam eu magna massa, sagittis accumsan est. Aenean sapien nisi, varius scelerisque scelerisque non, adipiscing at velit. Nulla arcu magna, dictum in tristique vel, egestas eget velit. Etiam elit augue, iaculis nec malesuada nec, scelerisque ut justo. Curabitur scelerisque blandit rhoncus. Aliquam aliquet lectus ac nisi fringilla in varius mauris blandit. Proin ullamcorper tincidunt dictum. Vivamus ullamcorper consectetur mi, ut convallis ligula gravida eget. Curabitur gravida elementum odio vel rutrum.
-</p></div>
-<div class="portfolio">
-<h2>Portfolio</h2>
-[Portfolio]
-</div>
-<div class="workshops">
-<h2>Workshops</h2>
-<p>-DreadFactory Treffen 2012
-</p></div>
+
 <?php
 }
